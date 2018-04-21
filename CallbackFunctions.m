78fe62f4a69911d1d9da6c819ffaf993c9beeadb
Mihai <mihai@icylines.com>
2018-04-21 15:17:53 +0100

    Recalculating the network output is no longer necessary

--- CallbackFunctions.m
+++ CallbackFunctions.m
@@ -1,57 +1,25 @@
 classdef CallbackFunctions
    methods(Static)
-        function oneOutputPlot(trainer)
-            if mod(trainer.epoch, 1) ~= 0, return, end;            
+        function oneOutputPlot(trainer)            
             subplot(2, 1, 1);          
             hold on
             cla
-            plot([1:size(trainer.YTrain,1)], trainer.YTrain);
-            plot([1:size(trainer.YTrain,1)], trainer.network.calculateOutput(trainer.XTrain));                       
-            text(0.1, 0.1, trainer.status + " Cost: " + num2str(trainer.totalCostHistory(1, end)));
-            title("Pattern vs. fit")
-            xlabel("Input")
+            dataSize = size(trainer.YTrain,1);
+            plot(1:dataSize, trainer.YTrain);
+            plot(1:dataSize, trainer.lastOutput);                       
+            title("Pattern vs. fit");
+            xlabel(trainer.status);
             ylabel("Output")
-            hold off
-            disp("Epoch: " + trainer.epoch + " Cost:" + num2str(trainer.totalCostHistory(1, end)));
+            hold off            
             subplot(2, 1, 2);
             plot(trainer.batchCostHistory);
-            hold on
-            plot(trainer.totalCostHistory);
+            hold on;
+            %plot(trainer.totalCostHistory);
             title("Total and batch costs");
             xlabel("Epoch");
             ylabel("Cost");
-            hold off
+            hold off;
             drawnow();
-        end
-        
-        
-        function twoOutputPlot(trainer)
-            if mod(trainer.epoch, 10) ~= 0, return, end;            
-            subplot(2, 1, 1);          
-            hold on
-            cla
-            YCalc = trainer.network.calculateOutput(trainer.XTrain);
-            [X,Y] = meshgrid(trainer.YTrain(:, 1), trainer.YTrain(:, 2));
-            mesh(X, Y, Z);
-            
-            [X,Y] = meshgrid(YCalc(:, 1), YCalc(:, 2), trainer.XTrain);
-            mesh(X);           
-            text(0.1, 0.1, trainer.status + " Cost: " + num2str(trainer.totalCostHistory(1, end)));
-            title("Pattern vs. fit")
-            xlabel("Input")
-            ylabel("Output 1")
-            ylabel("Output 2")
-            hold off
-            disp("Epoch: " + trainer.epoch + " Cost:" + num2str(trainer.totalCostHistory(1, end)));
-            subplot(2, 1, 2);
-            plot(trainer.batchCostHistory);
-            hold on
-            plot(trainer.totalCostHistory);
-            title("Total and batch costs");
-            xlabel("Epoch");
-            ylabel("Cost");
-            hold off
-            drawnow();
-        end
+        end                      
    end
 end
